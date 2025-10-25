cac21105bc12b6f44ef722609a0b8e6f7f6f528d
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-10-25 17:36:28 -0300

    adiciona timestamp e eventid no PagamentoConfirmadoEvent em nota-fiscal

--- nota-fiscal/src/main/java/mx/florinda/notafiscal/PagamentoConfirmadoEvent.java
+++ nota-fiscal/src/main/java/mx/florinda/notafiscal/PagamentoConfirmadoEvent.java
@@ -1,24 +1,26 @@
 package mx.florinda.notafiscal;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class PagamentoConfirmadoEvent {
+
+  public UUID eventId;
+  public LocalDateTime eventTimestamp;
+
   public Long pagamentoId;
   public Long pedidoId;
   public BigDecimal valor;
 
-  public PagamentoConfirmadoEvent(Long pagamentoId, Long pedidoId, BigDecimal valor) {
-    this.pagamentoId = pagamentoId;
-    this.pedidoId = pedidoId;
-    this.valor = valor;
-  }
-
   @Override
   public String toString() {
     return "PagamentoConfirmadoEvent{" +
-            "pagamentoId=" + pagamentoId +
-            ", pedidoId=" + pedidoId +
-            ", valor=" + valor +
-            '}';
+        "eventId=" + eventId +
+        ", eventTimestamp=" + eventTimestamp +
+        ", pagamentoId=" + pagamentoId +
+        ", pedidoId=" + pedidoId +
+        ", valor=" + valor +
+        '}';
   }
 }
